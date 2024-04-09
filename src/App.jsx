32183ab7d9b0252472ebca522fc5285074cf54ad
Jhonatan Estiven Becerra Pedrozo <jsbpdeveloper@gmail.com>
2024-04-09 07:53:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useCatImage } from "./hooks/useCatImage.js";
 import "./App.css";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
   const [fact, setFact] = useState();
   const { img } = useCatImage({ fact });
 
@@ -17,6 +18,8 @@ export default function App() {
 
   return (
     <main>
+      {inputText}
+      <input type="text" onChange={(e) => setInputText(e.target.value)} />
       {fact && <p>{fact}</p>}
       <button onClick={handleClick}>Refresh</button>
       {img && <img src={img} alt={`cats img`} />}
